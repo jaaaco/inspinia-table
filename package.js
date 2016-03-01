4c06eeaef835f2f2f936fd8cbd58763ff59e7307
Jakub Wietrzyk <jakub.wietrzyk@wfirma.pl>
2016-03-01 11:45:12 +0100

    predefined filters bugfix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'jaaaco:inspinia-table',
-    version: '0.0.16',
+    version: '0.0.17',
     // Brief, one-line summary of the package.
     summary: 'Reactive Tables with pagination, filters and more for Inspinia Admin Template (Bootstrap3)',
     // URL to the Git repository containing the source code for this package.
