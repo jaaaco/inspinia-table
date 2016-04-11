f4a783b2b065ebe32f800f1a5ffe433ae9ab3080
Jakub Wietrzyk <jakub.wietrzyk@wfirma.pl>
2016-04-11 11:52:41 +0200

    wersja 0.0.25

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'jaaaco:inspinia-table',
-    version: '0.0.22',
+    version: '0.0.25',
     // Brief, one-line summary of the package.
     summary: 'Reactive Tables with pagination, filters and more for Inspinia Admin Template (Bootstrap3)',
     // URL to the Git repository containing the source code for this package.
@@ -12,26 +12,27 @@ Package.describe({
 
 Package.onUse(function (api) {
     api.versionsFrom('1.2.1');
+    
+    api.addFiles('lib/Table.html', ['client']);
+    api.addFiles('lib/Table.js', ['client']);
+    api.addFiles('lib/Table.css', ['client']);
+
+    api.addFiles('lib/TableFilters.html', ['client']);
+    api.addFiles('lib/TableFilters.js', ['client']);
+
+    api.addFiles('lib/TablePagination.html', ['client']);
+    api.addFiles('lib/TablePagination.js', ['client']);
+
+    api.addFiles('lib/filters/predefined.html', ['client']);
+    api.addFiles('lib/filters/predefined.js', ['client']);
+
+    api.addFiles('lib/filters/select.html', ['client']);
+    api.addFiles('lib/filters/select.js', ['client']);
+
+    api.addFiles('lib/filters/button.html', ['client']);
+    api.addFiles('lib/filters/button.js', ['client']);
+
     api.use('blaze-html-templates','client');
     api.use('ecmascript');
     api.use('jaaaco:template-logic@0.0.3','client');
-
-    api.addFiles('lib/Table.html', 'client');
-    api.addFiles('lib/Table.js', 'client');
-    api.addFiles('lib/Table.css', 'client');
-
-    api.addFiles('lib/TableFilters.html', 'client');
-    api.addFiles('lib/TableFilters.js', 'client');
-
-    api.addFiles('lib/TablePagination.html', 'client');
-    api.addFiles('lib/TablePagination.js', 'client');
-
-    api.addFiles('lib/filters/predefined.html', 'client');
-    api.addFiles('lib/filters/predefined.js', 'client');
-
-    api.addFiles('lib/filters/select.html', 'client');
-    api.addFiles('lib/filters/select.js', 'client');
-
-    api.addFiles('lib/filters/button.html', 'client');
-    api.addFiles('lib/filters/button.js', 'client');
 });
